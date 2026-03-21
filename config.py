192ad330be9dff610cdedefcd3d2d4788b219f7a
olgaemail1978-rgb <olgaemail1978@gmail.com>
2026-03-21 20:31:32 +0200

    Add config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHILD_NAME = os.getenv("CHILD_NAME", "Ребёнок")
+NOTION_TOKEN = os.getenv("NOTION_TOKEN")
+NOTION_TRANSACTIONS_DB = os.getenv("NOTION_TRANSACTIONS_DB")
+NOTION_GOALS_DB = os.getenv("NOTION_GOALS_DB")
+ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
