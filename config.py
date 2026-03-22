bdab954574ea770aeefff042cd4b7b2ab0460780
olgaemail1978-rgb <olgaemail1978@gmail.com>
2026-03-22 14:47:35 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHILD_NAME = os.getenv("CHILD_NAME", "Ребёнок")
+CHILD_AGE = int(os.getenv("CHILD_AGE", "10"))
 NOTION_TOKEN = os.getenv("NOTION_TOKEN")
 NOTION_TRANSACTIONS_DB = os.getenv("NOTION_TRANSACTIONS_DB")
 NOTION_GOALS_DB = os.getenv("NOTION_GOALS_DB")
